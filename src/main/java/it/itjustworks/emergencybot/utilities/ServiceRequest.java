ab018cd9753decded24cb117b51ba8a8130ab4fd
Riccardo Crippa <therickys93@gmail.com>
2017-02-21 14:04:07 +0100

    use our backend

--- src/main/java/it/itjustworks/emergencybot/utilities/ServiceRequest.java
+++ src/main/java/it/itjustworks/emergencybot/utilities/ServiceRequest.java
@@ -12,28 +12,17 @@ import it.itjustworks.emergencybot.server.BotConstants;
 
 public class ServiceRequest {
 	
+	private static String EMERGENCY_SERVER_API_ENDPOINT = "https://emergency-server.herokuapp.com/api/";
+	
+	// GET /api/:latitude/:longitude
 	public String executeWithLocation(Location location) throws Exception{
-		Country country = getCountry("http://scatter-otl.rhcloud.com/location?lat="+ location.latitude().toString() + "&long=" + location.longitude().toString());
-		if (country == null) {
-			return BotConstants.GETTING_COUNTRY_ERROR;
-		}
-		Emergency emergency = getEmergency("https://emergency-phone-numbers.herokuapp.com/country/" + country.getCode());
+		Emergency emergency = getEmergency(EMERGENCY_SERVER_API_ENDPOINT + location.latitude().toString() + "/" + location.longitude().toString());
 		if(emergency == null) {
 			return BotConstants.GETTING_EMERGENCY_ERROR;
 		}
 		return emergency.toJSON();
 	}
-	
-	private Country getCountry(String url) throws Exception {
-		Client client = new Client(new Context(), Protocol.HTTP);
-		ClientResource clientResource = new ClientResource(url);
-		clientResource.setNext(client);
-		Representation data = clientResource.get();
-		Country country = new Country(data.getText());
-		client.stop();
-		return (country.getCode() == null) ? null : country;
-	}
-	
+
 	private Emergency getEmergency(String url) throws Exception {
 		Client client = new Client(new Context(), Protocol.HTTPS);
 		ClientResource clientResource = new ClientResource(url);
