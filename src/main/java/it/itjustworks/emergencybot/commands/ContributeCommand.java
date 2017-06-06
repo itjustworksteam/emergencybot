8a65f36a70c8b8b344ace6f19bd82d8005f4b68a
Riccardo Crippa <therickys93@gmail.com>
2017-06-06 09:03:05 +0200

    add /credits and /contribute in Italian

--- src/main/java/it/itjustworks/emergencybot/commands/ContributeCommand.java
+++ src/main/java/it/itjustworks/emergencybot/commands/ContributeCommand.java
@@ -1,13 +1,12 @@
 package it.itjustworks.emergencybot.commands;
 
+import it.itjustworks.emergency.Utils;
+
 public class ContributeCommand implements Command {
 
 	@Override
 	public String execute(String message, String language) {
-		String output = "";
-		output += "If you are a developer please follow this link: https://github.com/itjustworksteam/emergencybot/blob/master/README.md\n\n"
-				+ "if you have any suggestion please use the /feedback command.\nThanks so much for your help!\n";
-		return output;
+		return Utils.botLanguage(language).contribute();
 	}
 
 }
